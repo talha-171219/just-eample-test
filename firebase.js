7d87d0caa38842b24ed028109bbd38e931f0a252
talha-171219 <monirulhasantalha96@gmail.com>
2025-08-25 16:25:28 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB2PcLHyYtR-dOMUgKnqKF4dKTrUZRjf-I",
   authDomain: "chat-bot-ai-cc7cf.firebaseapp.com",
   projectId: "chat-bot-ai-cc7cf",
-  storageBucket: "chat-bot-ai-cc7cf.firebasestorage.app",
+  storageBucket: "chat-bot-ai-cc7cf.appspot.com",
   messagingSenderId: "949908800966",
   appId: "1:949908800966:web:61d644a9c66e01bf6e1ec7"
 };
